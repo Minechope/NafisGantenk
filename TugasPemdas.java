5b1c7b79d162ec5cc7b7bc6027b32f749ce0bd4f
Minechope <minechope@gmail.com>
2025-09-21 00:30:56 +0800

    first commit

---
+++ TugasPemdas.java
@@ -0,0 +1,13 @@
+public class TugasPemdas {
+    public static void main(String[] args) {
+        String nama = "Nafis";
+        int umur = 21;
+        boolean aktif = true;
+
+        System.out.println("=======================");
+        System.out.println("|| Nama  : " + nama + "     ||");
+        System.out.println("|| Umur  : " + umur + "        ||");
+        System.out.println("|| Aktif : " + aktif + "      ||");
+        System.out.println("=======================");
+    }
+}
